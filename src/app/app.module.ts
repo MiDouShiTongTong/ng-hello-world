c103c645863ac22a163d89de8991652c178e60b3
MiDouShiTongTong <i@yyccyy.com>
2018-11-21 11:56:20 +0800

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,67 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import {
+  NZ_I18N,
+  zh_CN,
+  NzGridModule,
+  NzMenuModule,
+  NzFormModule,
+  NzInputModule,
+  NzSelectModule,
+  NzButtonModule,
+  NzCarouselModule
+} from 'ng-zorro-antd';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchComponent } from './search/search.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { ProductComponent } from './product/product.component';
+import { StartsComponent } from './starts/starts.component';
+
+/** 配置 angular i18n **/
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+
+registerLocaleData(zh);
 
 @NgModule({
+  // 只能声明组件, 指令
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SearchComponent,
+    CarouselComponent,
+    ProductComponent,
+    StartsComponent
   ],
   imports: [
+    // 浏览器模块
     BrowserModule,
-    AppRoutingModule
+    // 路由模块
+    AppRoutingModule,
+    // 动画模块,
+    BrowserAnimationsModule,
+    // ng-zorro-antd 模块
+    NzGridModule,
+    NzMenuModule,
+    NzFormModule,
+    NzInputModule,
+    NzSelectModule,
+    NzButtonModule,
+    NzCarouselModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    // 配置 ng-zorro-antd 国际化
+    {
+      provide: NZ_I18N, useValue: zh_CN
+    }
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/starts/starts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-starts',
+  templateUrl: './starts.component.html',
+  styleUrls: ['./starts.component.scss']
+})
+export class StartsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
