44fdbfb2fcebb09ee3f1bb9b6f22c056395b6da4
MiDouShiTongTong <i@yyccyy.com>
2018-11-23 17:18:01 +0800

    依赖注入, 数据绑定, 响应式编程, 管道

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import { SearchOutline } from '@ant-design/icons-angular/icons';
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
 import {
   NZ_I18N,
   zh_CN,
@@ -13,12 +17,10 @@ import {
   NzButtonModule,
   NzCarouselModule,
   NzCardModule,
-  NzListModule
+  NzListModule,
+  NzIconModule,
+  NZ_ICONS
 } from 'ng-zorro-antd';
-
-/** 配置 angular i18n **/
-import { registerLocaleData } from '@angular/common';
-import zh from '@angular/common/locales/zh';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
@@ -29,12 +31,16 @@ import { ProductComponent } from './component/product/product.component';
 import { StartsComponent } from './component/starts/starts.component';
 import { ProductDetailComponent } from './component/product-detail/product-detail.component';
 import { HomeComponent } from './component/home/home.component';
+import { IconDefinition } from '@ant-design/icons-angular';
+import { FilterPipe } from './pipe/filter/filter.pipe';
 
 registerLocaleData(zh);
+const icons: IconDefinition[] = [ SearchOutline ];
 
 @NgModule({
   // 只能声明组件, 指令
   declarations: [
+    FilterPipe,
     AppComponent,
     HeaderComponent,
     FooterComponent,
@@ -52,6 +58,10 @@ registerLocaleData(zh);
     AppRoutingModule,
     // 动画模块,
     BrowserAnimationsModule,
+    // 表单模块
+    FormsModule,
+    // 响应式模块
+    ReactiveFormsModule,
     // ng-zorro-antd 模块
     NzGridModule,
     NzMenuModule,
@@ -61,13 +71,13 @@ registerLocaleData(zh);
     NzButtonModule,
     NzCarouselModule,
     NzCardModule,
-    NzListModule
+    NzListModule,
+    NzIconModule
   ],
   providers: [
     // 配置 ng-zorro-antd 国际化
-    {
-      provide: NZ_I18N, useValue: zh_CN
-    }
+    {provide: NZ_I18N, useValue: zh_CN },
+    { provide: NZ_ICONS, useValue: icons }
   ],
   bootstrap: [
     AppComponent
--- src/app/component/product/product.component.ts
+++ src/app/component/product/product.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../service/product/product.service';
+import { FormControl } from '@angular/forms';
+import { debounceTime} from 'rxjs/operators';
 import { Product } from 'src/app/model/Product';
 
 @Component({
@@ -8,12 +10,19 @@ import { Product } from 'src/app/model/Product';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
+  searchNameFormControl: FormControl = new FormControl();
+  searchName: string;
   productList: Product[];
 
   constructor(
     public productService: ProductService
   ) {
-
+    // 订阅 searchNameFormControl
+    this.searchNameFormControl.valueChanges
+      .pipe(
+        debounceTime(100)
+      )
+      .subscribe(value => this.searchName = value);
   }
 
   ngOnInit() {
